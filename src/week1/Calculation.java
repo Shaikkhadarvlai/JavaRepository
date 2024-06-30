b5de4064e4213d6f174afcf41992041c1dd53225
Shaikkhadarvlai <khadarsk414@gmail.com>
2024-06-30 16:40:13 +0530

    commitfirst

---
+++ src/week1/Calculation.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Calculation 
+{
+	static int a=10;
+	static int b=20;
+	static String c="khadar vali";
+	public static void main(String []ars)
+	{
+      System.out.println(a+b);
+      System.out.println(c);
+
+	}
+
+}
